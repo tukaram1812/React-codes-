2fa8f08b16e3a901b9bd02d6e8c093c7b8adefe4
Tukaram Shirse <tukaram1812@gmail.com>
2025-08-24 23:05:26 +0530

    episode-o1

---
+++ App.js
@@ -0,0 +1,47 @@
+// How to create nested Element in REACT
+
+/* 
+<div id="parent"> 
+    <div id="child">
+        <h1> i am h1 tag </h1>
+        <h2> i am h2 tag</h2>
+    </div>
+    <div id="child2">
+        <h1> i am h1 tag</h1>
+        <h2> i am h2 tag</h2>
+    </div>
+</div> 
+*/
+
+// ReactElement(object) = HTML (browser Understanding)
+
+// like below we create nested element in react
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "i am h1 tag"),
+    React.createElement("h2", {}, "i am h2 tag"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "i am h1 tag"),
+    React.createElement("h2", {}, "i am h2 tag"),
+  ]),
+]);
+
+// JSX = to avoid this bulky code USE  jsx
+
+console.log(parent);
+
+// ReactElement = object
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading", xyx: "abc" },
+//   "Hello World from React"
+// );
+// create a root to put all dom inside that
+// creating a root and rendering dom inside that is job of React DOM
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
